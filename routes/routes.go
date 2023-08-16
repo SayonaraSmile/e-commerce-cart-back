138738b9ba5902051e5962234301c7c1978ded78
SayonaraSmile <rezarifa660@gmail.com>
2023-08-16 16:13:32 +0700

    add routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"e-commerce-cart/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 // Handling routes
 func UserRoute(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("users/login")
-	incomingRoutes.POST("users/signup")
+	incomingRoutes.POST("/users/login", controllers.SignUp())
+	incomingRoutes.POST("/users/signup", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
