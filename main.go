eaa535ca6de209636facc6a62a50150f6532d3ac
SayonaraSmile <rezarifa660@gmail.com>
2023-08-16 19:48:13 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"e-commerce-cart/controllers"
+	"e-commerce-cart/database"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"),
+		database.UserData(database.Client("Users")))
 }
